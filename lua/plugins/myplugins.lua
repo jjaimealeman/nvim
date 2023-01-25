a0eb4f99ced588f29c78ad3ac859c20e8f515787
Jaime Aleman <jjaimealeman@gmail.com>
2023-01-25 14:42:17 -0700

    Add plugin copilot, vim-wakatime, comment, astro lsp, and tailwind-colorizer.

--- lua/plugins/myplugins.lua
+++ lua/plugins/myplugins.lua
@@ -1,5 +1,15 @@
 return {
-  { "zbirenbaum/copilot.lua" },
+  {
+    "zbirenbaum/copilot.lua",
+    lazy = false,
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      vim.schedule(function()
+        require("copilot").setup()
+      end)
+    end,
+  },
   {
     "Edeneast/nightfox.nvim",
     lazy = false, -- make sure we load this during setup if it is your maincolorscheme
@@ -13,5 +23,36 @@ return {
     "mbbill/undotree",
     lazy = true,
     cmd = { "UndotreeShow", "UndotreeToggle", "UndotreeHide", "UndotreeFocus" },
+  },
+  { "wakatime/vim-wakatime", lazy = false },
+  {
+    "roobert/tailwindcss-colorizer-cmp.nvim",
+    lazy = false,
+    -- optionally, override the default options:
+    config = function()
+      require("tailwindcss-colorizer-cmp").setup({
+        color_square_width = 2,
+      })
+      require("cmp").config.formatting = {
+        format = require("tailwindcss-colorizer-cmp").formatter,
+      }
+    end,
+  },
+
+  {
+    "numToStr/Comment.nvim",
+    lazy = false,
+    config = function()
+      require("Comment").setup()
+    end,
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      filetypes = {
+        "astro",
+      },
+    },
   },
 }

3d9b7d2327a3f8e290d7f22c410fe977c9298ad0
Jaime Aleman <jjaimealeman@gmail.com>
2024-04-28 16:34:37 -0600

    Add better keymap for switching tabs with SHIFT and SHIFT TAB.

--- lua/jjaime/core/keymaps.lua
+++ lua/jjaime/core/keymaps.lua
@@ -18,6 +18,8 @@ keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split." 
 
 keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab." })
 keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab." })
-keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to new tab." })
-keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab." })
+--keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab." })
+keymap.set("n", "<tab>", "<cmd>tabn<CR>", { desc = "Go to next tab." })
+--keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab." })
+keymap.set("n", "<s-tab>", "<cmd>tabp<CR>", { desc = "Go to previous tab." })
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab." })

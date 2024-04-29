0c469779910ec115c0fdc4760c1b9e73860548cc
Jaime Aleman <jjaimealeman@gmail.com>
2024-04-28 19:29:55 -0600

    Add surround.

---
+++ lua/jjaime/plugins/surround.lua
@@ -0,0 +1,20 @@
+return {
+  "kylechui/nvim-surround",
+  event = { "BufReadPre", "BufNewFile" },
+  version = "*", -- Use for stability; omit to use `main` branch for the latest features
+  config = true,
+}
+
+-- "Lorem" ipsum dolor dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
+
+--
+--     Old text                    Command         New text
+-- --------------------------------------------------------------------------------
+--     surr*ound_words             ysiw)           (surround_words)
+--     *make strings               ys$"            "make strings"
+--     [delete ar*ound me!]        ds]             delete around me!
+--     remove <b>HTML t*ags</b>    dst             remove HTML tags
+--     'change quot*es'            cs'"            "change quotes"
+--     <b>or tag* types</b>        csth1<CR>       <h1>or tag types</h1>
+--     delete(functi*on calls)     dsf             function calls
+--

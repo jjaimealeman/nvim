04cd4034b316bd14c0d64670208b73452361a8e2
Jaime Aleman <jjaimealeman@gmail.com>
2024-04-28 23:44:26 -0600

    Added opts.signs from other Lazyvim setup.

--- lua/jjaime/plugins/gitsigns.lua
+++ lua/jjaime/plugins/gitsigns.lua
@@ -2,6 +2,14 @@ return {
   "lewis6991/gitsigns.nvim",
   event = { "BufReadPre", "BufNewFile" },
   opts = {
+    signs = {
+      add = { text = "++" },
+      change = { text = "│+" },
+      delete = { text = "--" },
+      topdelete = { text = "^^" },
+      changedelete = { text = "│-" },
+      untracked = { text = "┆┆" },
+    },
     on_attach = function(bufnr)
       local gs = package.loaded.gitsigns
 

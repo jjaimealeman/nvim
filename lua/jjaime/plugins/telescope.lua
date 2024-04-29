33fef40f4898d8e1f3f3c9fda0f60821ed161da5
Jaime Aleman <jjaimealeman@gmail.com>
2024-04-29 11:34:44 -0600

    Add my own layout to telescope and resort the keybinds.

--- lua/jjaime/plugins/telescope.lua
+++ lua/jjaime/plugins/telescope.lua
@@ -13,12 +13,17 @@ return {
 
     telescope.setup({
       defaults = {
-        path_display = { "smart" },
+        layout_strategy = "vertical",
+        layout_config = { height = 0.95 },
+        -- OPTIONS:
+        -- telescope.defaults.path_display
+        -- "hidden", "tail", "absolute", "smart", "shorten", "truncate"
+        path_display = { "truncate" },
         mappings = {
           i = {
             ["<C-k>"] = actions.move_selection_previous, -- move to prev result
             ["<C-j>"] = actions.move_selection_next, -- move to next result
-            -- FIX: this actions was causing an error.
+            -- BUG: this actions was causing an error.
             -- ["<C-q>"] = actions.send_selected_to_qflist + custom_actions.open_trouble_qflist,
           },
         },
@@ -30,11 +35,20 @@ return {
     -- set keymaps
     local keymap = vim.keymap -- for conciseness
 
+    keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { desc = "Find in buffers???" })
+    keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
     keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
+    keymap.set("n", "<leader>fg", "<cmd>Telescope git_files<cr>", { desc = "Fuzzy find files in `git ls-files`." })
+    keymap.set("n", "<leader><leader>", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
     keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
     keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
-    keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
     -- NOTE: this is how to search for `todo-comments` in code.
     keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
   end,
 }
+--
+-- TODO: check out
+-- :help telescope.layout
+-- https://github.com/nvim-telescope/telescope.nvim?tab=readme-ov-file#layout-display
+--
+--

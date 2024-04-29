eedf3e465f4e12f344e26f332e10893097b9e4f4
Jaime Aleman <jjaimealeman@gmail.com>
2024-04-28 19:29:59 -0600

    Add todo-comments.

---
+++ lua/jjaime/plugins/todo-comments.lua
@@ -0,0 +1,42 @@
+return {
+  "folke/todo-comments.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  dependencies = { "nvim-lua/plenary.nvim" },
+
+opts = {
+    TODO = { icon = " ", color = "info" },
+    HACK = { icon = " ", color = "warning" },
+    FIX = { icon = " ", color = "warning", alt = { "WARNING", "XXX", "BROKE", "BROKEN" } },
+    PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+    NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
+    TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
+  },
+
+  config = function()
+    local todo_comments = require("todo-comments")
+
+    -- set keymaps
+    local keymap = vim.keymap -- for conciseness
+
+    keymap.set("n", "]t", function()
+      todo_comments.jump_next()
+    end, { desc = "Next todo comment" })
+
+    keymap.set("n", "[t", function()
+      todo_comments.jump_prev()
+    end, { desc = "Previous todo comment" })
+
+    todo_comments.setup()
+  end,
+  -- TODO: do something.
+  -- HACK: weird code warning.
+  -- FIX: this code should be fixed.
+  --
+  -- NOTE:
+  -- 
+  -- "<leader>xt"
+  -- TEST:
+  -- RUN WITH:
+  -- "<leader>ft"
+}
+

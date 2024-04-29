445ba6203a00051886da9dcb4803aca546d7f665
Jaime Aleman <jjaimealeman@gmail.com>
2024-04-28 19:30:39 -0600

    Update lazy to import plugins.lsp.

--- lua/jjaime/lazy.lua
+++ lua/jjaime/lazy.lua
@@ -11,7 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("jjaime.plugins", {
+require("lazy").setup({ { import = "jjaime.plugins" }, { import = "jjaime.plugins.lsp" } }, {
 
 checker = {
   enabled = true,

ff77d17dd292e8325b256d295b4b046eda86af5e
Jaime Aleman <jjaimealeman@gmail.com>
2024-04-29 11:32:21 -0600

    Fix button sorting and add load stats in alpha.

--- lua/jjaime/plugins/alpha.lua
+++ lua/jjaime/plugins/alpha.lua
@@ -19,16 +19,34 @@ return {
 
     -- Set menu
     dashboard.section.buttons.val = {
-      dashboard.button("SPC ff", "󰱼  > Find File", "<cmd>Telescope find_files<CR>"),
-      dashboard.button("SPC ee", "  > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
-      dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
-      dashboard.button("SPC fs", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
-      dashboard.button("SPC wr", "󰁯  > Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
-      dashboard.button("q", "  > Quit NVIM", "<cmd>qa<CR>"),
+      dashboard.button("f", "󰱼    Find File", "<cmd>Telescope find_files<CR>"),
+      dashboard.button("t", "    Toggle tree explorer", "<cmd>NvimTreeToggle<CR>"),
+      dashboard.button("n", "    New File", "<cmd>ene<CR>"),
+      dashboard.button("g", "    Find Word", "<cmd>Telescope live_grep<CR>"),
+      dashboard.button("r", "󰁯    Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
+      dashboard.button("l", "󰒲    Load Lazy", "<cmd> Lazy <cr>"),
+      dashboard.button("q", "    Quit NVIM", "<cmd>qa<CR>"),
     }
 
     -- Send config to alpha
     alpha.setup(dashboard.opts)
+
+    vim.api.nvim_create_autocmd("User", {
+      once = true,
+      pattern = "LazyVimStarted",
+      callback = function()
+        local stats = require("lazy").stats()
+        local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+        dashboard.section.footer.val = "⚡ Neovim loaded "
+          .. stats.loaded
+          .. "/"
+          .. stats.count
+          .. " plugins in "
+          .. ms
+          .. "ms"
+        pcall(vim.cmd.AlphaRedraw)
+      end,
+    })
 
     -- Disable folding on alpha buffer
     vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])

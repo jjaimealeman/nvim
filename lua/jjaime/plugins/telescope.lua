3a8c4cfe3aa4b87986c5f2562c1e8618bc4a65ee
Jaime Aleman <jjaimealeman@gmail.com>
2024-04-29 19:08:02 -0600

    Remapped <leader><leader> to git_files in telescope.

--- lua/jjaime/plugins/telescope.lua
+++ lua/jjaime/plugins/telescope.lua
@@ -39,7 +39,7 @@ return {
     keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
     keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
     keymap.set("n", "<leader>fg", "<cmd>Telescope git_files<cr>", { desc = "Fuzzy find files in `git ls-files`." })
-    keymap.set("n", "<leader><leader>", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
+    keymap.set("n", "<leader><leader>", "<cmd>Telescope git_files<cr>", { desc = "Fuzzy find files in cwd" })
     keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
     keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
     -- NOTE: this is how to search for `todo-comments` in code.

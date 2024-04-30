3464f5805a73794a66d141a8256616c2344491a7
Jaime Aleman <jjaimealeman@gmail.com>
2024-04-29 19:08:22 -0600

    Fixed lualine and used my own catppuccin colors.

--- lua/jjaime/plugins/lualine.lua
+++ lua/jjaime/plugins/lualine.lua
@@ -5,22 +5,28 @@ return {
     local lualine = require("lualine")
     local lazy_status = require("lazy.status") -- to configure lazy pending updates count
 
+    -- NOTE:
+    -- USE THIS AS THE COLOR GUIDE!
+    -- https://github.com/catppuccin/catppuccin/blob/main/docs/translation-table.md
     local colors = {
-      blue = "#65D1FF",
-      green = "#3EFFDC",
-      violet = "#FF61EF",
-      yellow = "#FFDA7B",
-      red = "#FF4A4A",
-      fg = "#c3ccdc",
-      bg = "#112638",
-      inactive_bg = "#2c3043",
+      sapphire1 = "#04A5E5",
+      sapphire2 = "#99D1DB",
+      green = "#40A02B",
+      mauve = "#CA9EE6",
+      yellow = "#DF8E1D",
+      maroon = "#E64553",
+      fg = "#CCD0DA",
+      bg = "#181825",
+      inactive_bg = "#24273A",
     }
 
     local my_lualine_theme = {
       normal = {
-        a = { bg = colors.blue, fg = colors.bg, gui = "bold" },
+        a = { bg = colors.sapphire1, fg = colors.bg, gui = "bold" },
         b = { bg = colors.bg, fg = colors.fg },
         c = { bg = colors.bg, fg = colors.fg },
+        y = { bg = colors.sapphire2, fg = colors.bg },
+        z = { bg = colors.sapphire1, fg = colors.bg },
       },
       insert = {
         a = { bg = colors.green, fg = colors.bg, gui = "bold" },
@@ -28,7 +34,7 @@ return {
         c = { bg = colors.bg, fg = colors.fg },
       },
       visual = {
-        a = { bg = colors.violet, fg = colors.bg, gui = "bold" },
+        a = { bg = colors.mauve, fg = colors.bg, gui = "bold" },
         b = { bg = colors.bg, fg = colors.fg },
         c = { bg = colors.bg, fg = colors.fg },
       },
@@ -38,7 +44,7 @@ return {
         c = { bg = colors.bg, fg = colors.fg },
       },
       replace = {
-        a = { bg = colors.red, fg = colors.bg, gui = "bold" },
+        a = { bg = colors.maroon, fg = colors.bg, gui = "bold" },
         b = { bg = colors.bg, fg = colors.fg },
         c = { bg = colors.bg, fg = colors.fg },
       },
@@ -53,18 +59,62 @@ return {
     lualine.setup({
       options = {
         theme = my_lualine_theme,
+        icons_enabled = true,
+        component_separators = { left = "", right = "" },
+        section_separators = { left = "", right = "" },
+        -- section_separators = { left = "", right = "" },
+        -- component_separators = { left = "", right = "" },
       },
+      -- sections = {
+      --   lualine_x = {
+      --     {
+      --       lazy_status.updates,
+      --       cond = lazy_status.has_updates,
+      --       color = { fg = "#ff9e64" },
+      --     },
+      --     { "encoding" },
+      --     { "fileformat" },
+      --     { "filetype" },
+      --   },
+      -- },
       sections = {
-        lualine_x = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch", "diff", "diagnostics" },
+
+        --lualine_c = { "filename", path = 1 },
+        -- https://github.com/nvim-lualine/lualine.nvim?tab=readme-ov-file#filename-component-options
+        lualine_c = {
           {
-            lazy_status.updates,
-            cond = lazy_status.has_updates,
-            color = { fg = "#ff9e64" },
+            "filename",
+            file_status = true, -- Displays file status (readonly status, modified status)
+            newfile_status = false, -- Display new file status (new file means no write after created)
+            path = 1, -- 0: Just the filename
+            -- 1: Relative path
+            -- 2: Absolute path
+            -- 3: Absolute path, with tilde as the home directory
+            -- 4: Filename and parent dir, with tilde as the home directory
+
+            -- shorting_target = 40, -- Shortens path to leave 40 spaces in the window
+            -- for other components. (terrible name, any suggestions?)
+            symbols = {
+              modified = "[+]", -- Text to show when the file is modified.
+              readonly = "[-]", -- Text to show when the file is non-modifiable or readonly.
+              unnamed = "[No Name]", -- Text to show for unnamed buffers.
+              newfile = "[New]", -- Text to show for newly created file before first write
+            },
           },
-          { "encoding" },
-          { "fileformat" },
-          { "filetype" },
         },
+        lualine_x = { "filetype" },
+        lualine_y = { "progress" },
+        lualine_z = { "location" },
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { "filename" },
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {},
       },
     })
   end,

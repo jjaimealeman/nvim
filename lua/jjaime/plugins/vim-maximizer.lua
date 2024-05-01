2b6de5a723f76d064846ff4b5f4b953af2daeb60
Jaime Aleman <jjaimealeman@gmail.com>
2024-04-30 21:36:53 -0600

    Add note to vim-maximizer.

--- lua/jjaime/plugins/vim-maximizer.lua
+++ lua/jjaime/plugins/vim-maximizer.lua
@@ -1,6 +1,8 @@
 return {
   "szw/vim-maximizer",
   keys = {
-    { "<leader>sm", "<cmd>MaximizerToggle<CR>", desc = "Maximize/minimize a split" }
-  }
+    -- NOTE:
+    -- leader sm doesnt make sense, maybe have all "random" extensions under the same X?
+    { "<leader>sm", "<cmd>MaximizerToggle<CR>", desc = "Maximize/minimize a split" },
+  },
 }

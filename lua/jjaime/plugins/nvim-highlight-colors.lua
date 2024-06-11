bece952a7f85daac43ee705d89a47a6ed6057b6d
Jaime Aleman <jjaimealeman@gmail.com>
2024-06-10 21:42:16 -0600

    Add nvim-highlight-colors.

---
+++ lua/jjaime/plugins/nvim-highlight-colors.lua
@@ -0,0 +1,21 @@
+return {
+  "brenoprata10/nvim-highlight-colors",
+  config = function()
+    require("nvim-highlight-colors").setup({
+      ---Render style
+      ---@usage 'background'|'foreground'|'virtual'
+      -- render = "foreground",
+      render = "background",
+      -- render = "virtual",
+
+      ---Set virtual symbol (requires render to be set to 'virtual')
+      virtual_symbol = "■",
+
+      ---Highlight named colors, e.g. 'green'
+      enable_named_colors = true,
+
+      ---Highlight tailwind colors, e.g. 'bg-blue-500'
+      enable_tailwind = true,
+    })
+  end,
+}

9a819123cc7fffcbb92108f9bf85effd5175466a
Jaime Aleman <jjaimealeman@gmail.com>
2025-03-12 22:01:10 -0600

    Fix background_colour nvim-notify.

--- lua/jjaime/plugins/nvim-notify.lua
+++ lua/jjaime/plugins/nvim-notify.lua
@@ -2,7 +2,7 @@ return {
   "rcarriga/nvim-notify",
   config = function()
     require("notify").setup({
-      background_colour = "181825",
+      background_colour = "#181825",
     })
   end,
 }

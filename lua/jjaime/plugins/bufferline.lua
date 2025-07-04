3b9a9b381e771356278dbd6539ef151e7a592c8d
Jaime Aleman <jjaimealeman@gmail.com>
2025-07-04 00:45:50 -0600

    make buffers more like open files with simple tabs up top

--- lua/jjaime/plugins/bufferline.lua
+++ lua/jjaime/plugins/bufferline.lua
@@ -6,8 +6,45 @@ return {
   version = "*",
   opts = {
     options = {
-      mode = "tabs",
-      -- separator_style = "slant",
+      mode = "buffers",
+      separator_style = "line",
+      themable = true,
+      show_buffer_close_icons = true,
+      show_close_icon = true,
+      show_tab_indicators = true,
+      persist_buffer_sort = true,
+      diagnostics = "nvim_lsp",
+      diagnostics_update_in_insert = false,
+      offsets = {
+        {
+          filetype = "NvimTree",
+          text = "File Explorer",
+          text_align = "left",
+          separator = true,
+        },
+      },
+    },
+    highlights = {
+      buffer_selected = {
+        bold = true,
+        italic = false,
+        underline = true,
+      },
+      close_button_selected = {
+        bold = true,
+      },
+      modified_selected = {
+        bold = true,
+      },
+      duplicate_selected = {
+        bold = true,
+      },
+      separator_selected = {
+        bold = true,
+      },
+      indicator_selected = {
+        bold = true,
+      },
     },
   },
 }
